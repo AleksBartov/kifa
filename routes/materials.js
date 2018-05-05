d0826aa7bf37a9c981023be8fd15cef8e5539b72
Aleksandr <aleksandrbartov@yandex.ru>
2018-05-06 02:34:10 +0400

    now we are able to get, delet, put and post materials

--- routes/materials.js
+++ routes/materials.js
@@ -16,8 +16,34 @@ router.post('/', async (req, res) => {
 });
 
 router.get('/', async (req, res) => {
-    const materials = await Material.find().sort('name').select('name -_id');
+    const materials = await Material.find().sort('name');
     res.send(materials);
+});
+
+router.put('/:id', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+    const material = await Material.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        description: req.body.description
+    }, {new: true});
+    if (!material) return res.status(404).send('у нас нет таких материалов');
+
+    res.send(material);
+});
+
+router.delete('/:id', async (req, res) => {
+    const material = await Material.findByIdAndRemove(req.params.id);
+    if (!material) return res.status(404).send('у нас нет таких материалов');
+
+    res.send(material);
+});
+
+router.get('/:id', async (req, res) => {
+    const material = await Material.findById(req.params.id);
+    if (!material) return res.status(404).send('у нас нет таких материалов');
+
+    res.send(material);
 });
 
 module.exports = router;
